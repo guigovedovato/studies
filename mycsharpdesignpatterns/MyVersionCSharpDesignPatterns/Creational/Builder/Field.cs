8d9f5872db4586ed389dedfb7b82767472b9b2e0
Rodrigo Vedovato <guigovedovato@gmail.com>
2018-01-15 16:05:47 +0000

    builder

---
+++ mycsharpdesignpatterns/MyVersionCSharpDesignPatterns/Creational/Builder/Field.cs
@@ -0,0 +1,10 @@
+﻿namespace MyVersionCSharpDesignPatterns.Creational.Builder
+{
+    public class Field
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString() => $"public {Type} {Name}";
+    }
+}
