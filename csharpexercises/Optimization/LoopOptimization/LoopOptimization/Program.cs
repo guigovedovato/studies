3340f69991b89edeb10b51b8b90b9bfadcff5651
Rodrigo Vedovato <guigovedovato@gmail.com>
2018-01-18 16:23:36 +0000

    loop optimization

---
+++ csharpexercises/Optimization/LoopOptimization/LoopOptimization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace LoopOptimization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = 100001;
+            var sb = new StringBuilder();
+            for (int i = 1; i < length; i++)
+            {
+                bool div5 = i % 5 == 0;
+                bool div7 = i % 7 == 0;
+                if (div5 && div7)
+                    sb.Append("FiveAndSeven, ");
+                else if (div5)
+                    sb.Append(i == (length - 1) ? "Five" : "Five, ");
+                else if (div7)
+                    sb.Append("Seven, ");
+                else
+                    sb.Append(i + ", ");
+            }
+            Console.Write(sb.ToString());
+            Console.WriteLine();
+        }
+    }
+}
