075b3dfe7239538dc7845057d22d1425558b70ba
Rodrigo Vedovato <guigovedovato@gmail.com>
2018-01-25 12:49:38 +0000

    Fibonacci static method

---
+++ csharpexercises/Fibonacci/Fibonacci .NetCore/Fibonacci/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace FibonacciSerie
+{
+    public class Fibonacci
+    {
+        public static string CalculateSerie(int serie)
+        {
+            if ((serie == 0) || (serie == 1)) return "0";
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < serie; i++)
+            {
+                sb.Append(FibonacciSeries(i));
+                sb.Append(" ");
+            }
+
+            sb.Length--;
+            return sb.ToString();
+        }
+
+        private static int FibonacciSeries(int num)
+        {
+            return ((num == 0) || (num == 1)) ? num : 
+                FibonacciSeries(num - 1) + FibonacciSeries(num - 2);
+        }
+    }
+}
