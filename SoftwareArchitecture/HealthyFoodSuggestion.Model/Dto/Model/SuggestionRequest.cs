8c29f967bc4cb223034592343007a122a51f466f
guigovedovato <guigovedovato@gmail.com>
2020-04-11 10:59:06 +0100

    added annotations

--- SoftwareArchitecture/HealthyFoodSuggestion.Model/Dto/Model/SuggestionRequest.cs
+++ SoftwareArchitecture/HealthyFoodSuggestion.Model/Dto/Model/SuggestionRequest.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HealthyFoodSuggestion.Model.Dto.Model
 {
     public class SuggestionRequest
     {
+        [Required]
         public byte Type { get; set; }
+        [Required]
         public string ingredient { get; set; }
     }
 }
