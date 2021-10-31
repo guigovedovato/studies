c297cc0655c78d41f4e8d576bd34bacf9690eebb
Rodrigo Vedovato <ext.rodrigo.vedovato@farfetch.com>
2021-10-31 13:56:29 +0000

    added class convention

--- Python/Console/Algorithms/projects/BorrowBooks/customer.py
+++ Python/Console/Algorithms/projects/BorrowBooks/customer.py
@@ -2,34 +2,33 @@ from person import Person
 
 
 class Customer(Person):
-          
-    __maxReadingBooks = (2,)
-           
+
     def __init__(self, name):
         super().__init__(name)
-        self.__booksReading = 0  
+        self._booksReading = 0  
+        self._maxReadingBooks = 2
     
     def request_book(self):
-        self.__set_number_of_reading_books()
+        self._set_number_of_reading_books()
         self.book = input("Enter the book name: ")
         return self.book  
     
     def return_book(self):
-        self.__release_number_of_reading_books()
+        self._release_number_of_reading_books()
         self.book = input("Enter the book name which is returning: ")
         return self.book  
     
     def get_reading_books(self):
-        return self.__booksReading   
+        return self._booksReading   
     
     def check_reading_books(self):
-        return self.__booksReading < self.__maxReadingBooks[0]   
+        return self._booksReading < self._maxReadingBooks[0]   
     
     def check_borrowed_books(self):
-        return self.__booksReading > 0  
+        return self._booksReading > 0  
     
-    def __set_number_of_reading_books(self):
-        self.__booksReading += 1
+    def _set_number_of_reading_books(self):
+        self._booksReading += 1
               
-    def __release_number_of_reading_books(self):
-        self.__booksReading -= 1
+    def _release_number_of_reading_books(self):
+        self._booksReading -= 1
