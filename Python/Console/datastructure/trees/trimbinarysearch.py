616117538b6649435157083435083b56ba05f002
Rodrigo Vedovato <ext.rodrigo.vedovato@farfetch.com>
2021-11-11 18:15:25 +0000

    added tests

--- Python/Console/datastructure/trees/trimbinarysearch.py
+++ Python/Console/datastructure/trees/trimbinarysearch.py
@@ -1,4 +1,5 @@
 from node import Node
+import treelevelorder
 
 
 # Given the root of a binary search tree and 2 numbers min and max, trim the tree such that all the numbers in the new tree are between min and max (inclusive). 
@@ -21,4 +22,15 @@ def trimBST(tree, minVal, maxVal):
         return tree.left
 
 
+tree = Node(1)
+tree.left = Node(2)
+tree.right = Node(3)
+tree.left.left = Node(4)
+tree.left.right = Node(5)
+tree.right.left = Node(6)
+tree.right.right = Node(7)
 
+print("Tree")
+treelevelorder.level_order_print(tree)
+print("Tree Trim")
+treelevelorder.level_order_print(trimBST(tree,1,5))
